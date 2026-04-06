450ef23c5ad7a5ea7c3012124c0f4beb4bb08c36
smtabhi66-art <smtabhi66@gmail.com>
2026-04-06 15:37:58 +0000

    Data types basics

---
+++ 01_basics/02data_types.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 )  we are using node.js not browser
+
+console.log(3
+    +
+    3) // code readability should be higher
+
+let name = "Abhinav"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
