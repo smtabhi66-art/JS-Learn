baa1db30e02fb06f252a41d180c6ee7e294e07bf
smtabhi66-art <smtabhi66@gmail.com>
2026-04-06 16:09:31 +0000

    different operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,33 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let showNumber = 33
 
 let stringNumber = String(showNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** OPERATIONS ***************** //
+
+let val = 3
+let negvalue = -val
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Abhinav"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// // console.log("1"+"2")
+// console.log("1" + 2 + 2);
+// console.log(1+2+"3");
+
+console.log(+true)
+
 
