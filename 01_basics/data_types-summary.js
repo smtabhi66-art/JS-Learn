fd223d84c36f601e4d1dd75002aec195c048f24d
smtabhi66-art <smtabhi66@gmail.com>
2026-04-12 00:41:31 +0000

    datatypes_summary

---
+++ 01_basics/data_types-summary.js
@@ -0,0 +1,18 @@
+//Primitive
+// 7 types : String,Number,Boolean,null,undefined,symbol,BigInt
+
+
+// Refernce (Non Primitive)
+
+// Array,Object,Function 
+
+const heros = ["shaktiman","nagraj","doga"]
+{
+    name: "abhinav",
+    age = 22;
+}
+ 
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
