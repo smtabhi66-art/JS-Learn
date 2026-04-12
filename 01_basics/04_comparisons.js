3b2a875f197f764e30bf6c6996908c43b01d4309
smtabhi66-art <smtabhi66@gmail.com>
2026-04-12 00:28:24 +0000

    comparisons equalities

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+ // during comparison like >,<,>=,<= the null is converted to 0 and then compared 
+ // its not done when we check for equality with == 
+console.log(undefined >0);
+
+// above types of comparison are avoided as not a good thing 
+
+// === (for striclyt checking like also the data types )
+
+console.log("2" === 2);
