e5abc7ed33096c7e25c6a54838085c7b712950b8
smtabhi66-art <smtabhi66@gmail.com>
2026-04-12 00:53:34 +0000

    Memory

--- 01_basics/data_types-summary.js
+++ 01_basics/data_types-summary.js
@@ -16,3 +16,30 @@ const myFunction = function(){
     console.log("Hello World");
 }
 console.log(typeof myFunction);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)  -> (Memory)
+
+
+let myChannelName = "abhinavSharma"
+
+let anotherName = myChannelName
+
+anotherName = "abhinav"
+console.log(myChannelName);
+
+console.log(anotherName);
+
+let user1 = {
+    email : "user@gmail.com",
+    upi : "user@eby"
+}
+
+let user2 = user1
+
+user2.email = "abhinav@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
+
