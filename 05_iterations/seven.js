80ec3632da84f198f8c5d5df2f61c801da607d45
smtabhi66-art <smtabhi66@gmail.com>
2026-04-17 03:26:42 +0000

    map methos usage

---
+++ 05_iterations/seven.js
@@ -0,0 +1,16 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map((num)=>{return num+10})
+
+// it's not a key value pair map it is array method
+// used to apply some operation and return new array with the change or operated values
+
+
+// const newNums = myNumbers
+//     .map((num) => num * 10)
+//     .map((num) => num + 1)
+//     .filter((num) => num >= 40)
+
+const myTotal = myNums.reduce
+
+console.log(newNums); 
